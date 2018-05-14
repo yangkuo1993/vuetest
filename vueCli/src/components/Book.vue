275efb54ab2fc61756fe0c824a823346a3f4f019
yangkuo <734271234@qq.com>
2018-05-14 16:53:59 +0800

    组件化

---
+++ vueCli/src/components/Book.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="book" @click='chooseBook(ISBN)'>
+    <h1>{{ name }}</h1>
+    <p>{{author}}</p>
+    <p>{{price}}</p>
+    <p>{{ISBN}}</p>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Book',
+  props: {
+    name: {
+      type: String,
+      require: true
+    },
+    author: {
+      type: String,
+      require: true
+    },
+    price: {
+      type: Number,
+      default: 100
+    },
+    ISBN: {
+      type: String,
+      require: true,
+      validator (value) {
+        return ['1', '2', '3'].indexOf(value) !== -1
+      }
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    chooseBook (ISBN) {
+      this.$emit('ISBN', ISBN)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.book{
+    display: inline-block;
+    border: 1px solid #f0f0f0;
+    background: #f0f00f;
+    vertical-align: top;
+}
+</style>
--- vueCli/src/components/HelloWorld.vue
+++ vueCli/src/components/HelloWorld.vue
@@ -1,113 +1,29 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <Book name='javascript' author='nobody' :price='200' ISBN='1' @ISBN='getISBN'></Book>
+    <Book name='javascript1' author='nobody' ISBN='2' @ISBN='getISBN'></Book>
+    <Book name='javascript2' author='nobody' ISBN='3' @ISBN='getISBN'>与众不同</Book>
   </div>
 </template>
 
 <script>
+import Book from './Book'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    Book
+  },
+  methods: {
+    getISBN (value) {
+      console.log(value)
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
